17f35cc8469a8f5a06cfcb2221f111e95362d28f
Deyb <112176170+dackydeyb@users.noreply.github.com>
2024-04-04 03:58:15 +0800

    Optimized Header, footer, and main content

--- JS/final.js
+++ JS/final.js
@@ -19,3 +19,60 @@ function transform(section) {
 
   scrollSection.style.transform = `translate3d(${-(percentage)}vw, 0, 0)`
 }
+
+
+
+/* 
+ScrollTrigger.create({
+  animation:gsap.from(".logo", {
+    y:"50vh",
+    scale:4,
+    yPercent:-50
+  }),
+  scrub:true,
+  trigger: ".container-2",
+  start: "top bottom",
+  endTrigger: '.container-2',
+  end: 'top center',
+  markers: true,
+  pin: true,
+  pinSpacing: false
+}); */
+
+/* Hamburger Menu Checkbox */
+document.addEventListener('DOMContentLoaded', function() {
+  const menuCheckbox = document.querySelector('.hamburger input[type="checkbox"]');
+  const menuText = document.querySelector('.hamburger .menu-text');
+
+  menuCheckbox.addEventListener('change', function() {
+    if (menuCheckbox.checked) {
+      menuText.textContent = 'CLOSE';
+    } else {
+      menuText.textContent = 'MENU';
+    }
+  });
+});
+
+/* Navigation Bar Hide On Scroll */
+// Get the navbar
+var navbar = document.querySelector('.navbar');
+
+// Get the current position of the scroll
+var lastScrollTop = 0;
+
+window.addEventListener("scroll", function() {
+   var currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+   if (currentScroll > lastScrollTop) {
+       // Scroll Down
+       navbar.style.top = "-60px"; // Adjust this value to the height of your navbar
+   } else {
+       // Scroll Up
+       navbar.style.top = "0px";
+   }
+
+   lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
+}, false);
+
+
+/* Parallax Footer */
